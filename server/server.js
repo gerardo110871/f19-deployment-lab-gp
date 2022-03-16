24a9df9dc3fba334e8553888c627d7f2356594a8
Gerardo <gpadil@icloud.com>
2022-03-16 13:10:10 -0600

    lets deploy

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`We are live on ${port}`);
+});
