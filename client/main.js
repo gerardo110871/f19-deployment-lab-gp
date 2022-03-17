4af4ddc7ab5b2199785de3bd24ff5b9eff95d8e2
Gerardo <gpadil@icloud.com>
2022-03-17 15:48:54 -0600

    fixed typo on nameinput

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ console.log('loaded diapers')
 function submit() {
     let nameInput = document.querySelector('input')
     
-    axios.post('/name', {name: nameinput.value})
+    axios.post('/name', {name: nameInput.value})
     .then(res => {
         nameInput.value = ""
     })
