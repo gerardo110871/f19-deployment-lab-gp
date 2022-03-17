a778644ae2e0382d1db2420c98e1ede5f99950b5
Gerardo <gpadil@icloud.com>
2022-03-17 14:27:32 -0600

    added rollbar message

--- server/server.js
+++ server/server.js
@@ -7,9 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '88262ca9e4b84ef0a0e8f16cf5be656a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 // middleware
 app.use(express.static(path.join(__dirname, '../client')))
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+app.use('/js', express.static(path.join(__dirname, 'client/main.js')))
 app.use("/styles", express.static(path.join(__dirname, '../client/index.css')))
 app.use("/favicon", express.static(path.join(__dirname, '../favicon/favicon.ico')))
 
@@ -20,7 +31,7 @@ app.use("/favicon", express.static(path.join(__dirname, '../favicon/favicon.ico'
 // app.get('/favicon', function (req, res) {
 //     res.sendFile(path.join(__dirname, '../favicon/favicon.ico'))
 // })
-
+ 
 
 //server port info
 const port = process.env.PORT || 4005;
